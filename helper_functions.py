d9e9ec26596cf92473ef1b65ec820ac4a37fd699
Zaetsfit <jrixjf@gmail.com>
2020-04-20 20:04:13 +0300

    New funcs bytestobit and bitstoint

--- helper_functions.py
+++ helper_functions.py
@@ -20,6 +20,7 @@ def int_to_bit(intnumber, bin_length=None):
 
     return padded_res
 
+
 def bit_to_byte(bits):
     """
     WILL BE CHANGED to convert bit sequences to bytes taking max sequence width into account.
@@ -48,6 +49,33 @@ def bit_to_byte(bits):
 
     return res_number
 
+
+def byte_to_bit(seq_bytes):
+    bits = []
+
+    for bt in seq_bytes:
+        value = bt
+
+        for one_more in range(8, 0, -1):
+            idk_bit = one_more - 1
+            next_b = 1 & (value >> idk_bit)
+            bits.append(next_b)
+
+    return bits
+
+
+def bits_to_int(bits):
+    num = 0
+    b_in_bits = [b for b in bits]
+    b_in_bits.reverse()
+
+    for idk_bit in range(len(b_in_bits)):
+        if b_in_bits[idk_bit]:
+            num = num | (1 << idk_bit)
+
+    return num
+
+
 def get_bytes(filepath):
     bytes = []
     file = open(filepath, 'rb')
@@ -57,4 +85,3 @@ def get_bytes(filepath):
         bytes.append(byte)
 
     return bytes
-
