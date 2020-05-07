dbfe8945bde156f772de1255fe4063f629b2c6e0
Zaetsfit <jrixjf@gmail.com>
2020-05-07 23:10:14 +0300

    add decompressor

--- Archiving_service.py
+++ Archiving_service.py
@@ -1,4 +1,6 @@
 from Compressor import Compressor
+from Decompressor import Decompressor
+from Bit_unpacker import Bit_unpacker
 from Bit_packer import Bit_packer
 from helper_functions import get_bytes
 
@@ -11,8 +13,11 @@ class Archiving_service:
     def __init__(self, input_files):
         self.__target_path, *self.__files_to_compress = input_files
         self.__compressor = Compressor()
+        self.__decompressor = Decompressor()
         self.__bit_packer = Bit_packer(
             initial_lenght=self.__compressor.table_size)
+        self.__bit_unpacker = Bit_unpacker(
+            initial_length=self.__decompressor.table_size)
 
     def encode(self):
         pass
